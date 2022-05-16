f9bf6500dcb4fb88eea5750cb2ca31df8cd1f3f1
suim <spdlqjtn159@gmail.com>
2022-05-16 17:59:13 +0900

    연습

--- test.js
+++ test.js
@@ -1,8 +1,25 @@
-console.log('test');
-console.log(8*5*4);
+// console.log('test');
+// console.log(8*5*4);
 
-let a = [4, 5, 6, 7];
+// let a = [4, 5, 6, 7];
 
-a.forEach(i =>{
-  console.log(i);
+// a.forEach(i =>{
+//   console.log(i);
+// })
+
+const arrExample = ["피카츄", "라이츄", "파이리", "꼬부기", "버터풀", "야도란" , "피존투", "또가스", "우린", "서로", "모습은", "달라도"]
+
+arrExample.forEach(x => {
+  console.log(x)
 })
+// arrExample.push('엑')
+// let a = arrExample.unshift()
+// console.log(arrExample.indexOf('또가스'))
+// arrExample.splice(1, 2);
+// console.log(arrExample.slice(1,2))
+// let map = arrExample.map(x=>{return x})
+// console.log(map === arrExample)
+// console.log(arrExample.reduce((arr,cur,i) => {return arr += cur;}, '초기값'))
+// console.log(arrExample.join('또가스'))
+let c = arrExample.findIndex()
+console.log(arrExample)
