b23ea6b94457adbb1e0641198a26150241180d60
suim <spdlqjtn159@gmail.com>
2022-05-17 12:21:53 +0900

    test

--- test.js
+++ test.js
@@ -9,9 +9,9 @@
 
 const arrExample = ["피카츄", "라이츄", "파이리", "꼬부기", "버터풀", "야도란" , "피존투", "또가스", "우린", "서로", "모습은", "달라도"]
 
-arrExample.forEach(x => {
-  console.log(x)
-})
+// arrExample.forEach(x => {
+//   console.log(x)
+// })
 // arrExample.push('엑')
 // let a = arrExample.unshift()
 // console.log(arrExample.indexOf('또가스'))
@@ -21,5 +21,20 @@ arrExample.forEach(x => {
 // console.log(map === arrExample)
 // console.log(arrExample.reduce((arr,cur,i) => {return arr += cur;}, '초기값'))
 // console.log(arrExample.join('또가스'))
-let c = arrExample.findIndex()
-console.log(arrExample)
+// arrExample.findIndex(function(){if(arrExample.length > 5){console.log(arrExample)}})
+// console.log(arrExample.filter(function(){return arrExample.indexOf('또가스') < 2}))
+// console.log(arrExample.some((ele)=>ele.length % 3 === 0))
+// let count = 0;
+// console.log(arrExample.values().next())
+// for(let value of arrExample.values()){
+//   console.log(count)
+//   count++;
+//   console.log(value)
+// }
+// console.log(Array.from(arrExample))
+// console.log(arrExample === Array.from(arrExample))
+// console.log(arrExample.at(8))
+// console.log(arrExample.concat('에엑1'))
+// console.log(arrExample.concat('에엑2'))
+// console.log(arrExample.every())
+console.log(Array.isArray(arrExample))
